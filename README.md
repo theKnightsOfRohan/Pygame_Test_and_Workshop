34fba061d255b21773665513296e03357846dd83
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-05-14 18:47:00 -0700

    Minor edits.

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a project for me to learn how to use pygame and become more proficient in python.
+```
--- src/level.py
+++ src/level.py
@@ -13,7 +13,7 @@ class Level:
         # Sprite group setup:
         # - Visibile sprites are those that are rendered
         # - Obstacle sprites are those that the player can collide with (NOT including the player)
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
         
         self.create_map()
@@ -37,5 +37,21 @@ class Level:
     def run(self):
         # update and draw sprites
         self.visible_sprites.update()
-        self.visible_sprites.draw(self.display_surface)
-        debug(self.player.direction)
+        self.visible_sprites.custom_draw(self.player)
+        
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_width() / 2
+        self.half_height = self.display_surface.get_height() / 2
+        self.offset = pygame.math.Vector2(100, 200)
+        
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
+            
